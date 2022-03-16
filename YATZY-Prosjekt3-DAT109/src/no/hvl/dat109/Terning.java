cf7623ad398e49061ae965e74186bc868ef69dc7
Alfred Nordqvist <591331@stud.hvl.no>
2022-03-16 13:53:01 +0100

    First iteration

---
+++ YATZY-Prosjekt3-DAT109/src/no/hvl/dat109/Terning.java
@@ -0,0 +1,11 @@
+package no.hvl.dat109;
+
+public class Terning {
+
+	
+	public void trillTerning() {
+		for(int i; i<5; i++) {
+			
+		}
+	}
+}
---
+++ YATZY-Prosjekt3-DAT109/src/no/hvl/dat109/YatzySpill.java
@@ -0,0 +1,11 @@
+package no.hvl.dat109;
+
+public class YatzySpill {
+
+	private Blokk blokk;
+	private Spiller[] spiller;
+	private Terning[] terninger;
+	
+	
+	
+}
